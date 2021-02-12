30de62f7b6ff26d365f6d32d755a31bd6c3735fe
Superfan1995 <xf4@ualberta.ca>
2021-02-11 23:49:19 -0700

    first commit

---
+++ httpclient.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Do not use urllib's HTTP GET and POST mechanisms.
+# Write your own HTTP GET and POST
+# The point is to understand what you have to send and get experience with it
+
+import sys
+import socket
+import re
+# you may use urllib to encode data appropriately
+import urllib.parse
+
+def help():
+    print("httpclient.py [GET/POST] [URL]\n")
+
+class HTTPResponse(object):
+    def __init__(self, code=200, body=""):
+        self.code = code
+        self.body = body
+
+class HTTPClient(object):
+
+    #def get_host_port(self,url):
+    def connect(self, host, port):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((host, port))
+        return None
+
+    def pharse_url(self, url):
+        
+        port = 80
+        path = "/"
+
+        url1 = url.split('http://')
+        url2= url1[1].split('/')
+
+        url_hostport = url2[0].split(':')
+        
+        host = url_hostport[0]
+        if ( len(url_hostport) == 2 ):
+            port = url_hostport[1]
+
+        path_len = len(url2)
+        i = 1
+        while ( i < path_len):
+            path = path + url2[i] + '/'
+            i = i + 1
+
+        return host, int(port), path
+
+    def get_code(self, data):
+
+        code = 200
+        lines = data.split('\r\n')
+        first_line = lines[0].split(' ')
+        code = int(first_line[1])
+
+        return code
+
+    def get_headers(self,data):
+
+        response = data.split('\r\n\r\n')
+        return response[0]
+
+    def get_body(self, data):
+
+        response = data.split('\r\n\r\n')
+        return response[1]
+    
+    def sendall(self, data):
+        self.socket.sendall(data.encode('utf-8'))
+        
+    def close(self):
+        self.socket.close()
+
+    # read everything from the socket
+    def recvall(self, sock):
+        buffer = bytearray()
+        done = False
+        while not done:
+            part = sock.recv(1024)
+            if (part):
+                buffer.extend(part)
+            else:
+                done = not part
+        return buffer.decode('utf-8')
+
+    def GET(self, url, args=None):
+
+        host, port, path = self.pharse_url(url)
+
+        data = 'GET ' + path + ' HTTP/1.1\r\n'
+        data = data + 'Host: ' + host + '\r\n'
+        data = data + 'Connection: close\r\n'
+        data = data + '\r\n'
+        
+        self.connect(host, port)
+        self.sendall(data)
+        response = self.recvall(self.socket)
+        
+        # print the result
+        print(response)
+
+        self.close()
+
+        code = self.get_code(response)
+        body = self.get_body(response)
+        return HTTPResponse(code, body)
+
+    def POST(self, url, args=None):
+
+        content_length = 0
+        body = ""
+        host, port, path = self.pharse_url(url)
+
+        if (args != None):
+            i = 0
+            for arg in args:
+
+                if (i == 1):
+                    body = body + "&"
+                new_str = arg + "=" + args[arg]
+                body = body + new_str
+                i = 1
+            content_length = len(body)
+
+        self.connect(host, port)
+
+        data = 'POST ' + path + ' HTTP/1.1\r\n'
+        data = data + 'Host: ' + host + '\r\n'
+        data = data + 'Connection: close\r\n'
+        data = data + 'Content-Length: ' + str(content_length) + '\r\n'
+        data = data + '\r\n'
+
+        if (len(body) != 0):
+            data = data + body
+            data = data + '\r\n'
+
+        self.connect(host, port)
+        self.sendall(data)
+        response = self.recvall(self.socket)
+        
+        # print the result
+        print(response)
+
+        self.close()
+
+        code = self.get_code(response)
+        body = self.get_body(response)
+        return HTTPResponse(code, body)
+
+    def command(self, url, command="GET", args=None):
+        if (command == "POST"):
+            return self.POST( url, args )
+        else:
+            return self.GET( url, args )
+    
+if __name__ == "__main__":
+    client = HTTPClient()
+    command = "GET"
+    if (len(sys.argv) <= 1):
+        help()
+        sys.exit(1)
+    elif (len(sys.argv) == 3):
+        print(client.command( sys.argv[2], sys.argv[1] ))
+    else:
+        print(client.command( sys.argv[1] ))
